0b6a3e8545d1316533a16187799a27f1fac8084c
FieryBlackFox <ya.daria-belozerova@yandex.ru>
2017-04-30 15:30:26 +0300

    add new

---
+++ Rotating an array/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[N, N];
+            string[] str;
+            for (int i = 0; i < N; i++)
+            {
+                str = Console.ReadLine().Split();
+                for (int j = 0; j < N; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(str[j]);
+                }
+            }
+            Program p = new Program();
+            arr = p.ArraySwap(arr);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+        public int[,] ArraySwap(int[,] arr)
+        {
+            int N1 = (int)Math.Sqrt(arr.Length);
+            int[,] array1 = new int[N1, N1];
+            for (int i = N1 - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < N1; j++)
+                {
+                    if (N1 - 1 - i == j)
+                    {
+                        array1[j, N1 - 1 - i] = arr[N1 - 1 - i, j];
+                    }
+                    else if (i == j)
+                    {
+                        array1[N1 - 1 - j, i] = arr[N1 - 1 - j, i];
+                    }
+                    else
+                    {
+                        array1[j, i] = arr[N1 - 1 - i, j];
+                    }
+                }
+                
+            }
+            return array1;
+        }
+    }
+}
---
+++ Rotating an array/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Array9;
+
+namespace TestForArray9
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] n = new int[3, 3]{ {1, 2, 3},
+                                      {4, 5, 6},
+                                      {7, 8, 9} };
+            int[,] n1 = new int[3,3]{ {1, 4, 3},
+                                      {8, 5, 2},
+                                      {7, 6, 9} };
+            Program p = new Program();
+            int[,] n2 = p.ArraySwap(n);
+            CollectionAssert.AreEqual(n1, n2);
+        }/*int[,] n1 = new int[5, 5]{ {1, 2, 3, 4, 5},
+                                       {6, 7, 8, 9, 10}*/
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[,] n = new int[5, 5]{ {1, 2, 3, 4, 5},
+                                      {6, 7, 8, 9, 10},
+                                      {11, 12, 13, 14, 15},
+                                      {16, 17, 18, 19, 20},
+                                      {21, 22, 23, 24, 25}};
+            int[,] n1 = new int[5, 5]{ {1, 16, 11, 6, 5},
+                                      {22, 7, 12, 9, 2},
+                                      {23, 18, 13, 8, 3},
+                                      {24, 17, 14, 19, 4},
+                                      {21, 20, 15, 10, 25}};
+            Program p = new Program();
+            int[,] n2 = p.ArraySwap(n);
+            CollectionAssert.AreEqual(n1, n2);
+        }
+
+    }
+}
